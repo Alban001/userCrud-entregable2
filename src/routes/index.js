02c46dfb43d636e926a3c5df4948c23c06b48f5e
Elias Alban <Alban001@users.noreply.github.com>
2024-05-20 19:34:06 -0400

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const user  = require('./user.router')
+
 // colocar las rutas aquí
 
+router.use(user)
 
 module.exports = router;
---
+++ src/routes/user.router.js
@@ -0,0 +1,15 @@
+const { getAll, create, update, remove, getOne } = require('../controllers/user.controllers');
+const express = require('express');
+
+const userRouter = express.Router();
+
+userRouter.route("/users")
+		.get(getAll)
+		.post(create)
+      
+userRouter.route("/users/:id/")
+		.put(update)
+		.delete(remove)
+        .get(getOne)
+
+module.exports = userRouter;
